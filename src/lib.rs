949bcdd5b69398cb92aaf40638a28fce5140cae8
coskunergan <coskunergan@gmail.com>
2025-03-20 03:18:52 +0300

    code continue.

--- src/lib.rs
+++ src/lib.rs
@@ -6,15 +6,24 @@
 // there might be.  This is completely impossible with both Kconfig and the DT configs, since the
 // whole point is that we likely need to check for configs that aren't otherwise present in the
 // build.  So, this is just always necessary.
-#![allow(unexpected_cfgs)]
+//#![allow(unexpected_cfgs)]
 
 use log::warn;
+extern crate alloc;
+//use zephyr::time::{sleep, Duration};
 
-use zephyr::time::{sleep, Duration};
+use embassy_time::{Duration, Ticker};
 
 use zephyr::{
+    //device::gpio::{GpioPin, GpioToken},
+    embassy::Executor,
     raw::{GPIO_INPUT, GPIO_OUTPUT_ACTIVE, GPIO_PULL_DOWN},
 };
+
+use embassy_executor::Spawner;
+use static_cell::StaticCell;
+
+static EXECUTOR_MAIN: StaticCell<Executor> = StaticCell::new();
 
 #[no_mangle]
 extern "C" fn rust_main() {
@@ -24,43 +33,48 @@ extern "C" fn rust_main() {
 
     warn!("Starting blinky");
 
-    do_blink();
+    let executor = EXECUTOR_MAIN.init(Executor::new());
+    executor.run(|spawner| {
+        spawner.spawn(main(spawner)).unwrap();
+    })
 }
 
-#[cfg(dt = "aliases::led0")]
-fn do_blink() {
+#[embassy_executor::task]
+async fn main(spawner: Spawner) {
     warn!("Inside of blinky");
 
-    let mut led0 = zephyr::devicetree::aliases::led0::get_instance().unwrap();
+    let _ = spawner;
+
+    //let mut led_red = zephyr::devicetree::aliases::led0::get_instance().unwrap();
+    let mut led = zephyr::devicetree::labels::led::get_instance().unwrap();
     let mut button = zephyr::devicetree::labels::button::get_instance().unwrap();
     let mut gpio_token = unsafe { zephyr::device::gpio::GpioToken::get_instance().unwrap() };
 
-    if !led0.is_ready() {
+    if !led.is_ready() {
         warn!("LED is not ready");
         loop {}
     }
 
     unsafe {
-        led0.configure(&mut gpio_token, GPIO_OUTPUT_ACTIVE);
+        led.configure(&mut gpio_token, GPIO_OUTPUT_ACTIVE);
         button.configure(&mut gpio_token, GPIO_INPUT | GPIO_PULL_DOWN);
     }
-    let duration = Duration::millis_at_least(500);
+    //let duration = Duration::millis_at_least(500);
     loop {
         unsafe {
-            led0.toggle_pin(&mut gpio_token);
+            led.toggle_pin(&mut gpio_token);
         }
+
+        //unsafe { button.wait_for_low(&mut gpio_token).await };
 
         if unsafe { button.get(&mut gpio_token) } == true {
-           sleep(duration / 10);
+            //sleep(duration / 10);
+            let mut ticker = Ticker::every(Duration::from_millis(50));
+            ticker.next().await;
         } else {
-            sleep(duration);
+            //sleep(duration);
+            let mut ticker = Ticker::every(Duration::from_millis(500));
+            ticker.next().await;
         }
-        
     }
-}
-
-#[cfg(not(dt = "aliases::led0"))]
-fn do_blink() {
-    warn!("No leds configured");
-    loop {}
 }
